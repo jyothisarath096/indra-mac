b9ff6f9a0fc6e79889599833fc2679337b8e6b51
Jyothi Sarath <jyothisarath096@gmail.com>
2026-05-30 20:55:17 +0530

    Initial Commit

---
+++ INDRANode/INDRANodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  INDRANodeApp.swift
+//  INDRANode
+//
+//  Created by Kolaparthi Jyothi Sarath on 30/05/26.
+//
+
+import SwiftUI
+
+@main
+struct INDRANodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
